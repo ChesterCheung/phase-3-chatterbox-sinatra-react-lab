64b266437b4a4da3493bd5b6a0848eefce5c6986
Chester Cheung <cheung319@gmail.com>
2022-06-21 15:40:37 -0400

    Pass

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+  get '/messages' do 
+    messages = Message.all.order(created_at: :asc)
+    messages.to_json
+  end
+
+  post '/messages' do 
+    createMessage = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    createMessage.to_json
+  end
+
+  patch '/messages/:id' do 
+    patchMessage = Message.find(params[:id])
+    patchMessage.update(
+      body: params[:body]
+    )
+    patchMessage.to_json
+  end
+
+  delete '/messages/:id' do 
+    deleteMessage = Message.find(params[:id])
+    deleteMessage.destroy
+    deleteMessage.to_json
+  end
+
 end
